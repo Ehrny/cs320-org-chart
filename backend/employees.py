a87376aaebe637de3864a3480426c6a629c62a95
Adam Powell <apowell@umass.edu>
2020-09-23 22:42:50 -0400

    Mock getting employee info

---
+++ backend/employees.py
@@ -0,0 +1,79 @@
+import links
+
+def employee_by_id(company_id: int , employee_id: int, tree_depth: int):
+    # MOCK ENDPOINT, IGNORES EVERYTHING
+    if tree_depth == 0:
+        return {
+            "firstName" : "Abdul",
+            "lastName" : "Humphrey",
+            "positionTitle" : "Engineering Manager",
+            "companyName" : "Tiger Microsystems",
+            "isManager" : True,
+            "email" : "Abdul_Humphrey@tigermicrosystems.com",
+            "startDate" : "2010-09-22",
+            "manager": links.get("/employee/3/manager"),
+            "employees": links.get("/employee/3?treeDepth=1"),
+            "actions": {
+                "update": links.put("/employee/3"),
+                "delete": links.delete("/employee/3"), 
+            }
+        }
+    elif tree_depth == 1:
+          {
+            "firstName" : "Abdul",
+            "lastName" : "Humphrey",
+            "positionTitle" : "Engineering Manager",
+            "companyName" : "Tiger Microsystems",
+            "isManager" : True,
+            "employeeId": 3,
+            "email" : "Abdul_Humphrey@tigermicrosystems.com",
+            "startDate" : "2010-09-22",
+            "manager": links.get("/employee/3/manager"),
+            "employees": [
+                {
+                    "firstName" : "Mickey",
+                    "lastName" : "Whitney",
+                    "positionTitle" : "Engineer",
+                    "companyName" : "Tiger Microsystems",
+                    "isManager" : False,
+                    "employeeId" : 4,
+                    "email" : "Mickey_Whitney@tigermicrosystems.com",
+                    "startDate" : "2015-03-07",
+                    "manager": links.get("/employee/4/manager"),
+                    "employees": None,
+                    "actions": {
+                        "update": links.put("/employee/4"),
+                        "delete": links.delete("/employee/4"), 
+                    },
+                },
+                {
+                    "firstName" : "Demetrius",
+                    "lastName" : "Ho",
+                    "positionTitle" : "Engineer",
+                    "companyName" : "Tiger Microsystems",
+                    "isManager" : False,
+                    "employeeId" : 10,
+                    "email" : "Demetrius_Ho@tigermicrosystems.com",
+                    "startDate" : "2016-05-07",
+                    "manager": links.get("/employee/10/manager"),
+                    "employees": None,
+                    "actions": {
+                        "update": links.put("/employee/10"),
+                        "delete": links.delete("/employee/10"), 
+                    },
+                }
+            ],
+            "actions": {
+                "update": links.put("/employee/3"),
+                "delete": links.delete("/employee/3"), 
+            }
+        }
+
+def employee_manager_by_id(company_id: int, employee_id: int, levels: int, tree_depth: int):
+    pass
+
+def employee_by_auth_token(company_id: int, auth_token: str, tree_depth: int):
+    return employee_by_id(0, 0, tree_depth)
+
+def employee_manager_by_auth_token(company_id: int, auth_token: str, levels: int, tree_depth: int):
+    return employee_manager_by_id(0, 0, levels, tree_depth)
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+import employees
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'todo: everything'
+
+@app.route('company/<company_id>/employee/<employee_id>')
+def route_employee_by_id(company_id: int, employee_id: int):
+    tree_depth = request.args.get("treeDepth", 0)
+    return employees.employee_by_id(company_id, employee_id, tree_depth)
+
+@app.route('company/<company_id>/employee/<employee_id>/manager')
+def route_employee_manager_by_id(company_id: int, employee_id: int):
+    levels = request.args.get("levels", 1)
+    tree_depth = request.args.get("treeDepth", 0)
+    return employees.employee_manager_by_id(company_id, employee_id, levels, tree_depth)
+
+@app.route('company/<company_id>/current_employee/<auth_token>')
+def route_employee_by_auth_token(company_id: int, auth_token: str):
+    tree_depth = request.args.get("treeDepth", 0)
+    return employees.employee_by_auth_token(company_id, auth_token, tree_depth)
+
+@app.route('company/<company_id>/current_employee/<auth_token>/manager')
+def route_employee_by_auth_token(company_id: int, auth_token: str):
+    tree_depth = request.args.get("treeDepth", 0)
+    return employees.employee_manager_by_auth_token(company_id, auth_token, tree_depth)
