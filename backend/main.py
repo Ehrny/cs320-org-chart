8ceeacf2167dc308e25ce06058292710df159bc2
Adam Powell <apowell@umass.edu>
2020-10-02 11:54:29 -0400

    Correct type errors in flask endpoints

--- backend/main.py
+++ backend/main.py
@@ -7,30 +7,30 @@ app = Flask(__name__)
 def index():
     return 'todo: everything'
 
-@app.route('company/<company_id>/employee/<employee_id>')
-def route_employee_by_id(company_id: int, employee_id: int):
+@app.route('/company/<company_id>/employee/<employee_id>')
+def route_employee_by_id(company_id: str, employee_id: str):
     tree_depth = request.args.get("treeDepth", 0)
-    return employees.employee_by_id(company_id, employee_id, tree_depth)
+    return employees.employee_by_id(int(company_id), int(employee_id), tree_depth)
 
-@app.route('company/<company_id>/employee/<employee_id>/manager')
-def route_employee_manager_by_id(company_id: int, employee_id: int):
+@app.route('/company/<company_id>/employee/<employee_id>/manager')
+def route_employee_manager_by_id(company_id: str, employee_id: str):
     levels = request.args.get("levels", 1)
     tree_depth = request.args.get("treeDepth", 0)
-    return employees.employee_manager_by_id(company_id, employee_id, levels, tree_depth)
+    return employees.employee_manager_by_id(int(company_id), int(employee_id), levels, tree_depth)
 
-@app.route('company/<company_id>/current_employee/<auth_token>')
-def route_employee_by_auth_token(company_id: int, auth_token: str):
+@app.route('/company/<company_id>/current_employee/<auth_token>')
+def route_employee_by_auth_token(company_id: str, auth_token: str):
     tree_depth = request.args.get("treeDepth", 0)
-    return employees.employee_by_auth_token(company_id, auth_token, tree_depth)
+    return employees.employee_by_auth_token(int(company_id), int(auth_token), tree_depth)
 
-@app.route('company/<company_id>/current_employee/<auth_token>/manager')
-def route_employee_manager_by_auth_token(company_id: int, auth_token: str):
+@app.route('/company/<company_id>/current_employee/<auth_token>/manager')
+def route_employee_manager_by_auth_token(company_id: str, auth_token: str):
     tree_depth = request.args.get("treeDepth", 0)
     levels = request.args.get("levels", 1)
-    return employees.employee_manager_by_auth_token(company_id, auth_token, levels, tree_depth)
+    return employees.employee_manager_by_auth_token(int(company_id), auth_token, levels, tree_depth)
 
-@app.route('company/<company_id>/login', methods=['POST'])
-def route_login(company_id: int):
+@app.route('/company/<company_id>/login', methods=['POST'])
+def route_login(company_id: str):
     username = request.args.get("username", "")
     password = request.args.get("password", "")
-    return employees.login(company_id, username, password)
+    return employees.login(int(company_id), username, password)
