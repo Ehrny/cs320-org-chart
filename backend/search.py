5984d53e36b7befd47cd82bf2c91008e8a0f3814
Adam Powell <apowell@umass.edu>
2020-10-11 12:14:37 -0400

    Clean up api stuff

---
+++ backend/search.py
@@ -0,0 +1,20 @@
+"""
+Handles searching for employees.
+
+{
+        "_id" : ObjectId("5f76310f7b12743d1c83c6f6"),
+        "firstName" : "Miquel",
+        "lastName" : "Pineda",
+        "companyId" : 1,
+        "password" : "pinedami",
+        "positionTitle" : "CEO",
+        "companyName" : "Cyclone Aviation",
+        "isManager" : true,
+        "employeeId" : 1,
+        "email" : "Miquel_Pineda@cycloneaviation.com",
+        "startDate" : "2016-05-04"
+}
+
+"""
+
+def search()
