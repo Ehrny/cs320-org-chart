aaf1f47a4830f0cf7fb35af19797c0ae8430d19a
Adam Powell <apowell@umass.edu>
2020-10-11 11:39:36 -0400

    Commented out crashing bits of employees and main

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ def index():
     return 'todo: everything'
 
 app.run(debug=True)
-=======
+
 @app.route('/company/<company_id>/employee/<employee_id>')
 def route_employee_by_id(company_id: str, employee_id: str):
     tree_depth: str = request.args.get("treeDepth", "0")
@@ -31,8 +31,8 @@ def route_employee_manager_by_auth_token(company_id: str, auth_token: str):
     levels: str = request.args.get("levels", "1")
     return employees.employee_manager_by_auth_token(int(company_id), auth_token, int(levels), int(tree_depth))
 
-@app.route('/company/<company_id>/login', methods=['POST'])
-def route_login(company_id: str):
-    username: str = request.args.get("username", "")
-    password: str = request.args.get("password", "")
-    return employees.login(int(company_id), username, password)
+# @app.route('/company/<company_id>/login', methods=['POST'])
+# def route_login(company_id: str):
+#     username: str = request.args.get("username", "")
+#     password: str = request.args.get("password", "")
+#     return employees.login(int(company_id), username, password)
