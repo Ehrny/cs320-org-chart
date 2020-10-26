a63a3d58747256fe901ed38d9b76bdee6b3d7b6b
Fishyu1215 <58120435+Fishyu1215@users.noreply.github.com>
2020-10-26 17:00:40 -0400

    frontend update

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import {Login} from "./components/login"
 import {Dashboard} from "./dashboard"
 import {Dashboardemployee} from "./dashboardemployee"
+import {Dashboardmanager} from "./dashboardmanager"
 
 class App extends React.Component{
   constructor(props)
@@ -20,10 +21,11 @@ class App extends React.Component{
       <div className="login">
           <div className = "app-routes">
             <BrowserRouter>
-            <Switch>
+            <Switch>   
                 <Route path = "/login" component={Login} />
                 <Route path = "/Dashboard" component={Dashboard} />
                 <Route path = "/Dashboardemployee" component={Dashboardemployee} />
+                <Route path = "/Dashboardmanager" component={Dashboardmanager} />
             </Switch>
             </BrowserRouter>
             </div>
--- frontend/app/src/dashboardemployee.js
+++ frontend/app/src/dashboardemployee.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './imgs/logo.png';
+import Orgchart from '@unicef/react-org-chart'
 import 'antd/dist/antd.css';
+import Drop from "./drop";
 import './index.css';
 import './dashboard.css';
-import { Layout, Menu, Breadcrumb, Input, Select } from 'antd';
+import { Layout, Menu, Breadcrumb, Input, Select, Descriptions, Collapse, Modal, Button, Form, InputNumber ,DatePicker } from 'antd';
 import {
     DesktopOutlined,
     UserOutlined,
     LogoutOutlined,
     FundViewOutlined,
+    UploadOutlined
   } from '@ant-design/icons';
-  import { Descriptions } from 'antd';
-
-  import { Collapse } from 'antd';
-
-  import { Modal, Button } from 'antd';
-
-
-  import { Form,InputNumber} from 'antd';
+  
 
   const { Panel } = Collapse;
   function callback(key) {
@@ -33,8 +29,6 @@ import {
   function handleChange(value) {
     console.log(`selected ${value}`);
   }
-
-
 
 
   const onFinish = (values) => {
@@ -57,11 +51,26 @@ import {
 
 export class Dashboardemployee extends React.Component{
  
-
-
   
         state = {
           collapsed: false,
+          
+          downloadingChart: false,
+          config: {},
+          highlightPostNumbers: [1],
+          searchparam : 'First',
+        
+
+          firstName : '',
+          lastName : '',
+          companyId : 0,
+          password : '',
+          positionTitle : '',
+          companyName : '',
+          isManager : false,
+          employeeId : 0,
+          email : '',
+          startDate : '',
         };
       
         onCollapse = collapsed => {
@@ -70,27 +79,57 @@ export class Dashboardemployee extends React.Component{
         };
 
 
-        state = {
-          addEmp: false,
-          dropEmp: false,
-          editEmp: false,
+        handleChange = (searchparam) =>{
+          const sp = searchparam
+          this.setState({searchparam: sp})
+          console.log(`selected ${sp}`);
         };
-      
-        addEmp(modal1Visible) {
-          this.setState({ modal1Visible });
-        }
-      
-        dropEmp(modal2Visible) {
-          this.setState({ modal2Visible });
-        }
-        editEmp(modal3Visible) {
-          this.setState({ modal3Visible });
-        }
 
- 
+
+        handleSearch = (emplID) => {
+          console.log('First seach pass, searching by: ', this.state.searchparam)
+          if(this.state.searchparam=='EmplID')
+          {
+            console.log('searching by: ', this.state.searchparam)
+            let resp = fetch('/company/3/employee/' + emplID)
+            //let resp = fetch('/naivelogin/'+emplID)//company/3/employee/1')
+                            .then(response => {
+                              console.log(response);
+                              return response.json()})
+                            .then(json =>
+                              {
+                                console.log(json)
+                                this.setState({
+                                firstName : json.firstName,
+                                lastName : json.lastName,
+                                companyId : json.companyId,
+                                password : json.password,
+                                positionTitle : json.positionTitle,
+                                companyName : json.companyName,
+                                isManager : json.isManager,
+                                employeeId : json.employeeId,
+                                email : json.email,
+                                startDate : json.startDate})
+                              })
+          }
+          if(this.state.searchparam=='First')
+          {
+            console.log('Searching by firstname')
+          }
+          if(this.state.searchparam=='Last')
+          {
+            console.log("Searching by lastname")
+          }
+        }               
 
 
         render() {
+
+          const { tree, downloadingChart } = this.state
+          //For downloading org chart as image or pdf based on id
+          const downloadImageId = 'download-image'
+          const downloadPdfId = 'download-pdf'
+
           return (
             <Layout style={{ minHeight: '100vh' }}>
               <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
@@ -98,109 +137,6 @@ export class Dashboardemployee extends React.Component{
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                   
         <Menu.Item key="2" icon={<DesktopOutlined />}>View</Menu.Item>
-
-
-        
-        <SubMenu key="sub1" disabled icon={<UserOutlined />} title="Manage">
-                    
-        <Menu.Item key="3"onClick={()=>this.addEmp(true)}> add </Menu.Item> 
-                   
-          <Modal
-          title="Add Employee"
-          style={{ top: 20 }}
-          visible={this.state.modal1Visible}
-          onOk={() => this.addEmp(false)}
-          onCancel={() => this.addEmp(false)}
-        >
-         
-
-
-         <Form {...layout} name="nest-messages" onFinish={onFinish} >
-      <Form.Item
-        name={['user', 'name']}
-        label="Name"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={['user', 'email']}
-        label="Email"
-        rules={[
-          {
-            type: 'email',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={['user', 'EmployeeID']}
-        label="EmployeeID"
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item name={['user', 'positionTitle']} label="positionTitle">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'Tag']} label="Tag">
-        <Input.TextArea />
-      </Form.Item>
-      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-
-   
-
-
-
-
-
-
-
-
-
-        </Modal>
-
-
-
-       <Menu.Item key="4" onClick={()=>this.dropEmp(true)}>drop</Menu.Item>
-
-          <Modal
-          title="Drop Employee"
-          style={{ top: 20 }}
-          visible={this.state.modal2Visible}
-          onOk={() => this.dropEmp(false)}
-          onCancel={() => this.dropEmp(false)}
-        >
-          <p>contents...</p>
-      
-        </Modal>
-  
-
-        <Menu.Item key="5"onClick={()=>this.editEmp(true)}>edit</Menu.Item>
-
-          <Modal
-          title="Edit Employee"
-          style={{ top: 20 }}
-          visible={this.state.modal3Visible}
-          onOk={() => this.editEmp(false)}
-          onCancel={() => this.editEmp(false)}
-        >
-          <p>contents...</p>
-
-     
-        </Modal>
-        
-        </SubMenu>
-
                  
         <Menu.Item key="6" icon={<FundViewOutlined />}>Detail</Menu.Item>
                   
@@ -214,63 +150,58 @@ export class Dashboardemployee extends React.Component{
     </Sider>
 
            
+    <Layout className="site-layout">
 
-              <Layout className="site-layout">
+<Header className="site-layout-background" style={{ padding: 0 }} >
+  <div>          
 
-              <div className= 'imageContainer'>
-                <img src={logo} className='img' alt="logo" />   
-              </div>
+  <img src={logo} className='img' alt="logo" />
 
+    <Select className="selectColumn" defaultValue="First" onChange={this.handleChange} size="large">
+      <Option value="First" >First Name</Option>
+      <Option value="Last">Last Name</Option>
+      <Option value= "EmplID"> Employee ID</Option>
+      </Select>
 
+      <Search className="search-bar" placeholder="input search text" enterButton="Search" size="large"
+                    onSearch={value=>this.handleSearch(value)} /> 
+      <h1>
+        {this.state.firstName} {this.state.lastName}
+      </h1>
+  </div>
 
+</Header>
 
+<Content style={{ margin: '0 16px' }}>
+  <Breadcrumb style={{ margin: '16px 0' }}>
+  <Breadcrumb.Item>Matches found: </Breadcrumb.Item>
+  
+  </Breadcrumb>
+  <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
 
-                <Header className="site-layout-background" style={{ padding: 0 }} >
-                <div>          
-                  
-                  <Select className="selectColumn" defaultValue="First" onChange={handleChange} size="large">
-                    <Option value="First" >First Name</Option>
-                    <Option value="Last">Last Name</Option>
-                    </Select>
-    
-                    <Search className="search-bar" placeholder="input search text" enterButton="Search" size="large"
-                                  onSearch={value => console.log(value)} /> 
-                  </div>
-                </Header>
-
-                <Content style={{ margin: '0 16px' }}>
-                  <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>Org chart</Breadcrumb.Item>
-                  
-                  </Breadcrumb>
-                  <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                     tree
-                  </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>CS320 Team 4 Runtime Terror</Footer>
-            
-
-<dir className ="test" >
-    <hr></hr>
-   <Collapse defaultActiveKey={['1']} onChange={callback}>
-    <Panel header="Detail Info" key="1">
-      < Descriptions title="Employee Info" >
-    <Descriptions.Item label="First Name" bordered>...</Descriptions.Item>
-    <Descriptions.Item label="Last Name">...</Descriptions.Item>
-    <Descriptions.Item label="Position">...</Descriptions.Item>
-  </Descriptions>,
-    </Panel>
-    
-  </Collapse>,
- </dir>
+  </div>
+</Content>
+  <Footer style={{ textAlign: 'center' }}>
+    CS320 Team 4 Runtime Terror
+  </Footer>
 
 
-  </Layout>
-            </Layout>
+  <dir className ="test" >
+      <hr></hr>
+    <Collapse defaultActiveKey={['1']} onChange={callback}>
+    <Panel header="Detail Info" key="1" >
+<Descriptions Item style={{ textAlign: 'left' }}>
+<Descriptions.Item label="First Name" bordered>{this.state.firstName}</Descriptions.Item>
+<Descriptions.Item label="Last Name">{this.state.lastName}</Descriptions.Item>
+<Descriptions.Item label="Position">{this.state.positionTitle}</Descriptions.Item>
+<Descriptions.Item label="Company">{this.state.companyName}</Descriptions.Item>
+        </Descriptions>
+      </Panel>
+    </Collapse>
+  </dir>
+</Layout>
+</Layout>
+);
+}
+}
 
-
-
-          );
-        }
-      }
-      
