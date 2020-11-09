154e1dac59d4e8615eab311a8ff59982e6921fd9
Berk Erdal <berkerdal31@gmail.com>
2020-11-08 21:08:53 -0500

    typo

--- backend/employees.py
+++ backend/employees.py
@@ -119,6 +119,6 @@ def drop_employee_from_db(db: pymongo.MongoClient, employee_dict: dict):
 
 def edit_employee(db: pymongo.MongoClient, current_employee:dict, updated: dict):
     return db["Employees"].find_one_and_replace(current_employee, updated,
-                                        return_new_document = True)
+                                        returnNewDocument = True)
 
 
